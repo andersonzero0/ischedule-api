c81a390bd9604b284a00162ee9100dac17c20a82
Anderson Viana <anderson11viana@gmail.com>
2023-10-25 10:49:37 -0300

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,30 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
+import { AuthMiddleware } from './auth/auth.middleware';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    envFilePath: ".env"
-  }), PrismaModule, UserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    PrismaModule,
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+      consumer.apply(AuthMiddleware).forRoutes({
+        path: '*',
+        method: RequestMethod.ALL
+      })
+  }
+}
